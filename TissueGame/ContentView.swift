b8ea552e5b384faea1a21e43d82d6281f204c958
vazaluta <vazaluta@gmail.com>
2024-02-04 15:04:09 +0900

    Initial Commit

---
+++ TissueGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TissueGame
+//
+//  Created by Masato Sawada on 2024/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TissueGame/TissueGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TissueGameApp.swift
+//  TissueGame
+//
+//  Created by Masato Sawada on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct TissueGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
